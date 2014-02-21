9da19c68c702f5570f1a52ea265b369b587e1b3e
Xue Li <xue.li@hulu.com>
2014-02-21 18:40:02 +0800

    Fulfill basic functions

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Comparing two XML/HTML documents and output the differences.
     require 'nokogiri'
     require File.expand_path('../lib/xml',__FILE__)
 
-    doc1 = Nokogiri::XML('<divs type="array"><div><a>A</a><b>B</b></div><div><a>C</a><b>D</b></div></divs>')
-    doc2 = Nokogiri::XML('<divs type="array"><div><a>C</a><b>D</b></div><div><a>A</a><b>B</b></div></divs>')
+    doc1 = Nokogiri::XML("<divs type='array'><div><a>A</a><b>B</b></div><div><a>C</a><b>D</b></div></divs>")
+    doc2 = Nokogiri::XML("<divs type='array'><div><b>D</b><a>C</a></div><div><b>B</b><a>A</a></div></divs>")
 
     doc1.diff(doc2, {:added => true, :removed => true, :ordered => true, :array_ordered => true}) do |change,node|
       puts "#{change} #{node.to_html}".ljust(50) + node.parent.path
--- test.rb
+++ test.rb
@@ -4,7 +4,10 @@ require File.expand_path('../lib/xml',__FILE__)
 doc1 = Nokogiri::XML("<divs type='array'><div><a>A</a><b>B</b></div><div><a>C</a><b>D</b></div></divs>")
 doc2 = Nokogiri::XML("<divs type='array'><div><b>D</b><a>C</a></div><div><b>B</b><a>A</a></div></divs>")
 
-doc1.diff(doc2, {:added => true, :removed => false, :ordered => false, :array_ordered => true}) do |change,node|
+#doc1 = Nokogiri::XML(IO.read("browse_get_shows.xml"))
+#doc2 = Nokogiri::XML(IO.read("browse_get_shows1.xml"))
+
+doc1.diff(doc2, {:added => true, :removed => true, :ordered => true, :array_ordered => true}) do |change,node|
   puts "#{change} #{node.to_html}".ljust(50) + node.parent.path
 end
 #puts doc1.diff(doc2, {:added => true, :removed => true, :ordered => false, :array_ordered => false})
